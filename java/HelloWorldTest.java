1186ffbbadc54a1b5efeb66489386cf52b6cc94d
ZhuoHao Yuan <zhuohaoyuan1125@gmail.com>
2022-05-25 20:10:04 +1000

    inital

---
+++ java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+
+public class HelloWorldTest {
+   
+   public static void main(String[] args) {
+      System.out.println("5 - 1");
+      System.out.println("hey - 2");
+      System.out.println("hi - 1");
+      System.out.println("Mark - 2");
+      System.out.println("mark - 1");
+   }
+}
